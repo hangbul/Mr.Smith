efb7386093fffaa8ddd2264403041a1c72e82e7d
ljch0407 <ljch0407@gmail.com>
2022-03-19 21:36:12 +0900

    랜덤 넘버를 뽑아서 해당하는 유저의 스킬정보 불러오기

--- Assets/Scripts/phploader.cs
+++ Assets/Scripts/phploader.cs
@@ -5,7 +5,7 @@ using System.Net;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Networking;
-
+using Random = System.Random;
 
 public class phploader : MonoBehaviour
 {
@@ -14,7 +14,7 @@ public class phploader : MonoBehaviour
    private void Start()
    {
       StartCoroutine(getDBCount());
-    
+      StartCoroutine(getUserSkill());
    }
 
    //DB전체 count 받아옴
@@ -39,4 +39,36 @@ public class phploader : MonoBehaviour
          }
       }
    }
+
+   
+   //랜덤 넘버를 뽑기위한 함수
+   int getRandomNumber()
+   {
+      Random rand = new Random();
+      int result = rand.Next(1, DBCount);
+      return result;
+   }
+
+   IEnumerator getUserSkill()
+   {
+      WWWForm form = new WWWForm();
+      form.AddField ("UserNumberPost", getRandomNumber());
+
+      using (UnityWebRequest webRequest =
+             UnityWebRequest.Post("http://ljch0407.cafe24.com/LoadSkill.php", form))
+      {
+         yield return webRequest.SendWebRequest();
+
+         if (webRequest.isNetworkError || webRequest.isHttpError)
+         {
+            Debug.Log(webRequest.error);
+         }
+         else
+         {
+            string data = webRequest.downloadHandler.text;
+            Debug.Log(data);
+         }
+      } 
+   }
+
 }
