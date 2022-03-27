6b869b4334b29beaebd9e6549702694c48164374
Game_engine_Jun <wns0685@gmail.com>
2022-03-27 16:38:16 +0900

    Twitch Chat box 특정 유저의 로그만 읽어올 수 있으며, 2f초 후 사라짐

--- Assets/TwitchChatExample.cs
+++ Assets/TwitchChatExample.cs
@@ -5,37 +5,40 @@ using System.Collections.Generic;
 [RequireComponent(typeof(TwitchIRC))]
 public class TwitchChatExample : MonoBehaviour
 {
-    public int maxMessages = 100; 
-    public UnityEngine.RectTransform chatBox;
+    public GameObject chatBox;
     
     private TwitchIRC IRC;
     private GameObject go;
-    
     private UnityEngine.UI.Text text;
     private UnityEngine.UI.LayoutElement layout;
-    
+
     void OnChatMsgRecieved(string msg)
     {
-        //parse from buffer.
         int msgIndex = msg.IndexOf("PRIVMSG #");
         string msgString = msg.Substring(msgIndex + IRC.channelName.Length + 11);
         string user = msg.Substring(1, msg.IndexOf('!') - 1);
-
-        //add new message.
-        CreateUIMessage(user, msgString);
+        
+        if (user == "hangbul")
+        {
+            chatBox.SetActive(true);
+            CreateUIMessage(user, msgString);
+            Invoke("delay", 2);
+        }
+        
     }
+
     void CreateUIMessage(string userName, string msgString)
     {
-        Color32 c = ColorFromUsername(userName);
-        string nameColor = "#" + c.r.ToString("X2") + c.g.ToString("X2") + c.b.ToString("X2");
+        //Color32 c = ColorFromUsername(userName);
+        //string nameColor = "#" + c.r.ToString("X2") + c.g.ToString("X2") + c.b.ToString("X2");
         
-        go.transform.SetParent(chatBox);
+        go.transform.SetParent(chatBox.transform);
         go.transform.position = chatBox.transform.position;
         go.GetComponent<RectTransform>().anchorMin = new Vector2 (0.2f,0.5f);
         go.GetComponent<RectTransform>().anchorMax = new Vector2 (1-0.2f,0.5f);
 
         text.text = msgString;
-        text.color = Color.black;
+        //text.color = Color.black;
         text.fontSize = 20;
         text.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
     }
@@ -45,11 +48,10 @@ public class TwitchChatExample : MonoBehaviour
         Random.seed = username.Length + (int)username[0] + (int)username[username.Length - 1];
         return new Color(Random.Range(0.25f, 0.55f), Random.Range(0.20f, 0.55f), Random.Range(0.25f, 0.55f));
     }
-    // Use this for initialization
     void Start()
     {
+        chatBox.SetActive(false);
         IRC = this.GetComponent<TwitchIRC>();
-        //IRC.SendCommand("CAP REQ :twitch.tv/tags"); //register for additional data such as emote-ids, name color etc.
         go = new GameObject("twitchMsg");
         text = go.AddComponent<UnityEngine.UI.Text>();
         layout = go.AddComponent<UnityEngine.UI.LayoutElement>();
@@ -59,5 +61,10 @@ public class TwitchChatExample : MonoBehaviour
         text.fontSize = 20;
         text.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
         IRC.messageRecievedEvent.AddListener(OnChatMsgRecieved);
+    }
+
+    void delay()
+    {
+        chatBox.SetActive(false);
     }
 }
