a93743abbacaffce50e56a6442e0546489538fc2
ljch0407 <ljch0407@gmail.com>
2022-05-30 12:21:06 +0900

    공격오류 수정

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -75,10 +75,11 @@ public class Weapon : MonoBehaviour
         effect.Play();
         //traileEffect.enabled = true;
 
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.2f);
         effect.Stop();
 
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.2f);
+        meleeArea.enabled = false;
         //traileEffect.enabled = false;
         
     }
